366ed8db3053da1212a2feecbe137dcbefb52073
James O'Donoghue <jodonogh1@gmail.com>
2019-12-27 10:00:24 -0800

    added Firebase context for react HOC, update tsconfig

--- src/components/Firebase/firebase.ts
+++ src/components/Firebase/firebase.ts
@@ -10,8 +10,25 @@ const config = {
 };
 
 class Firebase {
+    auth: app.auth.Auth
+
     constructor() {
         app.initializeApp(config);
+
+        this.auth = app.auth();
     }
+
+    // *** Auth API ***
+    doCreateUserWithEmailAndPassword = (email: string, password: string) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email: string, password: string) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = (email: string) => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = (password: string) =>this.auth.currentUser.updatePassword(password);
 }
 export default Firebase;
