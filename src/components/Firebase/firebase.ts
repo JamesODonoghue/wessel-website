014ffc5f25b4f87a3871d112d79360411a620caa
James O'Donoghue <jodonogh1@gmail.com>
2020-01-01 11:22:54 -0800

    commented out firebase auth

--- src/components/Firebase/firebase.ts
+++ src/components/Firebase/firebase.ts
@@ -1,22 +1,17 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 
-const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-};
+// const config = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     databaseURL: process.env.REACT_APP_DATABASE_URL,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+// };
 
 class Firebase {
     auth: app.auth.Auth
-
-    constructor() {
-        app.initializeApp(config);
-        this.auth = app.auth();
-    }
 
     // *** Auth API ***
     doCreateUserWithEmailAndPassword = (email: string, password: string) =>
@@ -29,6 +24,6 @@ class Firebase {
 
     doPasswordReset = (email: string) => this.auth.sendPasswordResetEmail(email);
 
-    doPasswordUpdate = (password: string) =>this.auth.currentUser.updatePassword(password);
+    doPasswordUpdate = (password: string) => this.auth.currentUser.updatePassword(password);
 }
 export default Firebase;
